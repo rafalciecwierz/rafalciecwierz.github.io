52c20cde810b8c599ad66af5f7268dc59b62491f
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2020-05-03 18:33:34 +0200

    ADD: Scripts for header

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector(".navbar_bg");
+const sectionOne = document.querySelector(".front-text");
+const sectionOneOptions = {
+	rootMargin: "-50%",
+};
+// toDOO!
+const sectionOneObserver = new IntersectionObserver(
+	(entries, sectionOneObserver) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting)
+				header.classList.add("navbar_bg-scrolled");
+			else header.classList.remove("navbar_bg-scrolled");
+		});
+	},
+	sectionOneOptions
+);
+
+sectionOneObserver.observe(sectionOne);

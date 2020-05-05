df6ce6cda649560bdaef35f0a25660d4a7aadff9
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2020-05-05 16:02:56 +0200

    Add: Few new media queeries for mobile view

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const header = document.querySelector(".navbar_bg");
 const sectionOne = document.querySelector(".front-text");
+const sectionStart = document.querySelector(".front_logo ");
+const sectionAbout = document.querySelector(".about-section");
+const sectionOffer = document.querySelector(".section_offer");
+const sectionContact = document.querySelector(".contact-section");
+
 const sectionOneOptions = {
 	rootMargin: "-50% 0% 0% 0%",
 };
@@ -15,4 +20,23 @@ const sectionOneObserver = new IntersectionObserver(
 	sectionOneOptions
 );
 
+const activeStateObserver = new IntersectionObserver(
+	(entries, activeStateObserver) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+				// console.log("Poza obiektem");
+			} else {
+				console.log("W obiekcie");
+				console.log(entry.target.id);
+			}
+		});
+	},
+	{ rootMargin: "-200px" }
+);
+
 sectionOneObserver.observe(sectionOne);
+
+activeStateObserver.observe(sectionStart);
+activeStateObserver.observe(sectionAbout);
+activeStateObserver.observe(sectionOffer);
+activeStateObserver.observe(sectionContact);
